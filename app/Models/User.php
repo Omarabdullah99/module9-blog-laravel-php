e5d9b7b4129bd0069182801639f074818432d65c
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-07-26 17:03:51 +0600

    fillable and relationship create with User,Category and Post models e

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -22,6 +23,8 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+
+
 
     /**
      * The attributes that should be hidden for serialization.
@@ -44,5 +47,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+
+    public function posts():HasMany{
+        return $this->hasMany(Post::class);
     }
 }
