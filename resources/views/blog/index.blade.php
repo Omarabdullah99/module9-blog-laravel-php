03164069819413764b38c5671792feff45b9110b
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-07-28 02:38:15 +0600

    posts part dynamic resources>blog>index.blade.php er

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -5,61 +5,65 @@
     <section class="flex flex-col gap-8 px-4 py-20 mx-auto xl:gap-24 xl:grid xl:grid-cols-12 max-w-7xl sm:px-6 lg:px-8">
         <!-- Posts -->
         <section class="flex flex-col gap-8 xl:col-span-8">
-            <article
-                class="w-full rounded-xl p-6 text-white shadow-lg bg-zinc-900/20 ring-1 backdrop-blur-[2px] ring-white/15 space-y-6">
-                <div class="flex flex-row items-center justify-between">
-                    <!-- Category Pill -->
+            @forelse ($posts as $post)
+                <article
+                    class="w-full rounded-xl p-6 text-white shadow-lg bg-zinc-900/20 ring-1 backdrop-blur-[2px] ring-white/15 space-y-6">
+                    <div class="flex flex-row items-center justify-between">
+                        <!-- Category Pill -->
+                        <div>
+                            <a class="inline-flex gap-0.5 justify-center overflow-hidden text-sm sm:text-base font-medium transition rounded-full py-1 px-3 bg-emerald-400/10 text-emerald-400 ring-1 ring-inset ring-emerald-400/20 hover:bg-emerald-400/10 hover:text-emerald-300 hover:ring-emerald-300"
+                                href="{{ route('blog.index', ['category' => $post->category->slug]) }}">{{ $post->category->name }}
+                            </a>
+                        </div>
+
+                        <!-- Date -->
+                        <p class="text-sm font-semibold sm:text-base text-zinc-400">
+                            {{ $post->created_at->diffForHumans() }}
+                        </p>
+                    </div>
+
+                    <!-- Main Content -->
                     <div>
-                        <a class="inline-flex gap-0.5 justify-center overflow-hidden text-sm sm:text-base font-medium transition rounded-full py-1 px-3 bg-emerald-400/10 text-emerald-400 ring-1 ring-inset ring-emerald-400/20 hover:bg-emerald-400/10 hover:text-emerald-300 hover:ring-emerald-300"
-                            href="/#">Technology</a>
+                        <!-- Article Title -->
+                        <a href="{{ route('blog.show', $post->slug) }}">
+                            <h3 class="mb-4 text-xl font-bold md:text-4xl hover:underline decoration-emerald-700">
+                                {{ $post->title }}
+                            </h3>
+                        </a>
+
+                        <!-- excerpt -->
+                        <p class="text-base font-medium text-zinc-400 md:text-lg line-clamp-3">
+                            {{ str($post->body)->limit(250) }}
+                        </p>
                     </div>
 
-                    <!-- Date -->
-                    <p class="text-sm font-semibold sm:text-base text-zinc-400">
-                        4 months ago
-                    </p>
-                </div>
+                    <!-- Card Bottom -->
+                    <div class="flex flex-row items-center justify-between">
+                        <!-- Author Info -->
+                        <div class="flex items-center gap-2">
+                            <img class="w-8 h-8 p-0.5 rounded-full ring-1 ring-emerald-500"
+                                src="https://avatars.githubusercontent.com/u/108593907?v=4" alt="{{ $post->user->name }}" />
+                            <h4>{{ $post->user->name }}</h4>
+                        </div>
 
-                <!-- Main Content -->
-                <div>
-                    <!-- Article Title -->
-                    <a href="./pages/article.html">
-                        <h3 class="mb-4 text-xl font-bold md:text-4xl hover:underline decoration-emerald-700">
-                            The Case of the Disappearing Dish Towels
-                        </h3>
-                    </a>
-
-                    <!-- excerpt -->
-                    <p class="text-base font-medium text-zinc-400 md:text-lg line-clamp-3">
-                        Believing a rogue squirrel had taken up residence in my toaster oven, I fashioned a tiny helmet out
-                        of an
-                        acorn and bravely wrestled the (slightly singed) culprit out with a pair of tongs, only to discover
-                        it was
-                        just my neighbor's escaped hamster looking for a midday snack.
-                    </p>
-                </div>
-
-                <!-- Card Bottom -->
-                <div class="flex flex-row items-center justify-between">
-                    <!-- Author Info -->
-                    <div class="flex items-center gap-2">
-                        <img class="w-8 h-8 p-0.5 rounded-full ring-1 ring-emerald-500"
-                            src="https://avatars.githubusercontent.com/u/61485238?v=4" alt="Al Nahian" />
-                        <h4>Al Nahian</h4>
+                        <!-- Read More Link -->
+                        <a class="inline-flex gap-0.5 justify-center overflow-hidden text-base font-medium transition text-emerald-400 hover:text-emerald-500"
+                            href="{{route('blog.show',$post->slug)}}">
+                            Read more
+                            <svg viewBox="0 0 20 20" fill="none" aria-hidden="true"
+                                class="mt-0.5 h-5 w-5 relative top-px -mr-1">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    d="m11.5 6.5 3 3.5m0 0-3 3.5m3-3.5h-9"></path>
+                            </svg>
+                        </a>
                     </div>
+                </article>
+            @empty
+                <h3 class="mb-4 text-xl font-bold md:text-4xl">
+                    The Case of the Disappearing Dish Towels
+                </h3>
+            @endforelse
 
-                    <!-- Read More Link -->
-                    <a class="inline-flex gap-0.5 justify-center overflow-hidden text-base font-medium transition text-emerald-400 hover:text-emerald-500"
-                        href="./pages/article.html">
-                        Read more
-                        <svg viewBox="0 0 20 20" fill="none" aria-hidden="true"
-                            class="mt-0.5 h-5 w-5 relative top-px -mr-1">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                d="m11.5 6.5 3 3.5m0 0-3 3.5m3-3.5h-9"></path>
-                        </svg>
-                    </a>
-                </div>
-            </article>
         </section>
 
         <!-- Sidebar Widgets -->
