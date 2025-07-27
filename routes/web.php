ec30cd9a4f28285362923b44598cb026225c6fa0
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-07-28 01:58:12 +0600

    blog single page desing

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('blog.index');
 });
 
+Route::get('/single', function () {
+    return view('blog.show');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
