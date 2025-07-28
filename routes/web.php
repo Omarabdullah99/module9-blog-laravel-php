cbbe2350e55c585e3dbca950c33c0df12c8e0cc6
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-07-28 19:43:04 +0600

    Route auth middleware fixed and CategoryRoute Added with  auth middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\FrontCategoryController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
@@ -14,7 +15,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')
+->prefix('admin/')
+->name('admin.')
+->group(function () {
+    Route::get('/categories', [CategoryController::class]);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
